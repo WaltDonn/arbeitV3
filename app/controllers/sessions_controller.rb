50495cc0edd83185a0b2f2d83b8768288e40c3c5
Walter Donnellan <Walt@Walters-MBP.wv.cc.cmu.edu>
2018-10-01 19:41:50 -0400

    update account enum and added flag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,10 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     elsif user
-      flash.now.alert = "Password invalid"
+      flash.now.alert = "Password invalid. Account enumeration: Flag{UTRIAUB6AXWT8ED}"
       render "new" 
     else
-      flash.now.alert = "Email invalid"
+      flash.now.alert = "Email/Password invalid"
       render "new"
     end
   end
